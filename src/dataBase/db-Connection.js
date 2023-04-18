db7ddf60b30b8c86faf101ad7dadbf8f021ddc85
Mohamed Nasser <87243604+Mohamed1320Nasser@users.noreply.github.com>
2023-04-18 02:45:50 +0200

    Update db-Connection.js

--- src/dataBase/db-Connection.js
+++ src/dataBase/db-Connection.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 module.exports.dbConnection = () => {
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
       console.log("Connected to Mongodb atls");
     })
