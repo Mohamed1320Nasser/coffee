404ddb1d940aa8bd51cbf02774f3dd21852e1fac
Mohamed Nasser <87243604+Mohamed1320Nasser@users.noreply.github.com>
2023-05-12 00:13:43 +0300

    Update user.api.js

--- src/component/users/user.api.js
+++ src/component/users/user.api.js
@@ -24,19 +24,19 @@ router
   .post(protectedRoutes, allowedTo("admin"), validation(userSchema), creatUser)
   .get(protectedRoutes, allowedTo("admin"), getUsers);
 router.get("/verfy-email", verifyEmail);
-router.get("/myProfile", protectedRoutes, getProfile)
+router.get("/myProfile", protectedRoutes, getProfile).patch("/changePassword", protectedRoutes, validation(changePassSchema), ChangePass);
 router
   .route("/:id")
   .get(protectedRoutes, allowedTo("admin"), getUser)
   .put(protectedRoutes, allowedTo("admin"), updUser)
   .delete(delUser, protectedRoutes, allowedTo("admin"));
 
-router.post("/signUp",validation(userSchema), SignUp);
-router.post("/signin",validation(loginSchema), Signin);
+router.post("/signUp", validation(userSchema), SignUp);
+router.post("/signin", validation(loginSchema), Signin);
 router.post("/Signout", Signout);
 router
   // .get("/myProfile", protectedRoutes, getProfile)
   .put("/updateProfile", protectedRoutes, updateProfile)
-  .patch("/changePassword", protectedRoutes ,validation(changePassSchema), ChangePass);
+
 
 module.exports = router;
